3697f2ca9a3ddb3db1b3845e55d448a7719adb2c
kurapica <kurapica.igas@gmail.com>
2017-03-24 17:17:35 +0800

    Fix the strsub

--- System/System.lua
+++ System/System.lua
@@ -22,7 +22,7 @@ do
 	strbyte         = string.byte
 	strchar         = string.char
 	strrep          = string.rep
-	strsub          = string.gsub
+	strgsub         = string.gsub
 	strupper        = string.upper
 	strlower        = string.lower
 	strtrim         = strtrim or function(s) return (s:gsub("^%s*(.-)%s*$", "%1")) or "" end
