ab0b41aabaa457e8f97d7b674e220997329c40d2
kurapica <kurapica.igas@gmail.com>
2018-05-21 23:54:16 +0800

    Remove the __ConfigField__

--- System/Configuration.lua
+++ System/Configuration.lua
@@ -134,7 +134,7 @@ PLoop(function(_ENV)
 	--			print(config.nolinebreak)
 	-- 		end
 	--
-	--		__ConfigField__( System.Web.ConfigSection.Controller, "jsonprovider", -FormatProvider)
+	--		__ConfigSection__( System.Web.ConfigSection.Controller, "jsonprovider", -FormatProvider)
 	-- 		function JsonProviderConfig(field, value, ...)
 	-- 			print("The new json provider is " .. value)
 	-- 		end
@@ -160,17 +160,29 @@ PLoop(function(_ENV)
         -- @param   stack                       the stack level
         -- @return  data                        the attribute data to be attached
         function AttachAttribute(self, target, targettype, owner, name, stack)
-        	local section = self[1]
-        	if self[2] then
-        		for k, v in pairs(self[2]) do
-        			if type(k) == "string" and (isenum(v) or isstruct(v)) then
-        				section.Field[k] = v
-        			else
-        				error("The field's type can only be enum or struct", stack + 1)
-        			end
-        		end
-        	end
-        	section.OnParse = section.OnParse + function(self, ...) return target(...) end
+        	if self[3] then
+        		local section = self[1]
+	        	local fldname = self[2]
+	        	section.Field[fldname] = self[3]
+
+	        	section.OnFieldParse = section.OnFieldParse + function(self, fld, val, ...)
+	        		if fld == fldname then
+	        			return target(fld, val, ...)
+	        		end
+	        	end
+        	else
+	        	local section = self[1]
+	        	if self[2] then
+	        		for k, v in pairs(self[2]) do
+	        			if type(k) == "string" and (isenum(v) or isstruct(v)) then
+	        				section.Field[k] = v
+	        			else
+	        				error("The field's type can only be enum or struct", stack + 1)
+	        			end
+	        		end
+	        	end
+	        	section.OnParse = section.OnParse + function(self, ...) return target(...) end
+	        end
         end
 
         -----------------------------------------------------------
@@ -184,52 +196,9 @@ PLoop(function(_ENV)
         -----------------------------------------------------------
         __Arguments__{ ConfigSection, Table/nil }
         function __new(_, section, fields)
-        	return { section, fields }, true
-        end
-	end)
-
-	__Sealed__() class "__ConfigField__" (function(_ENV)
-		extend "IAttachAttribute"
-
-		export {
-			pairs 				= pairs,
-			type 				= type,
-			strformat 			= string.format,
-			isenum 				= Enum.Validate,
-			isstruct 			= Struct.Validate,
-		}
-
-        -----------------------------------------------------------
-        --                        method                         --
-        -----------------------------------------------------------
-        --- attach data on the target
-        -- @param   target                      the target
-        -- @param   targettype                  the target type
-        -- @param   owner                       the target's owner
-        -- @param   name                        the target's name in the owner
-        -- @param   stack                       the stack level
-        -- @return  data                        the attribute data to be attached
-        function AttachAttribute(self, target, targettype, owner, name, stack)
-        	local section = self[1]
-        	local fldname = self[2]
-        	section.Field[fldname] = self[3]
-
-        	section.OnFieldParse = section.OnFieldParse + function(self, fld, val, ...)
-        		if fld == fldname then
-        			return target(fld, val, ...)
-        		end
-        	end
+        	return { section, fields, false }, true
         end
 
-        -----------------------------------------------------------
-        --                       property                        --
-        -----------------------------------------------------------
-        --- the attribute target
-        property "AttributeTarget"  { set = false, default = AttributeTargets.Function }
-
-        -----------------------------------------------------------
-        --                      constructor                      --
-        -----------------------------------------------------------
         __Arguments__{ ConfigSection, NEString, (EnumType + StructType)/Any }
         function __new(_, section, name, type)
         	return { section, name, type }, true
