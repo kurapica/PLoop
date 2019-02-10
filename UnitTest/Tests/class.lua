e2c83de321a8d034edf678fa03bda2b42401c439
kurapica <kurapica125@outlook.com>
2019-02-10 11:51:30 +0800

    update test case for class

--- UnitTest/Tests/class.lua
+++ UnitTest/Tests/class.lua
@@ -16,27 +16,246 @@ _ENV = UnitTest "PLoop.Class" "1.1.0"
 
 namespace "UnitTest.ClassCase"
 
+__Test__() function ctor()
+    class "User" (function(_ENV)
+        local created   = {}
 
+        field { name = "anonymous" }
 
-__Test__() function usage()
-	interface "IA" (function(_ENV)
-		__Static__() function ifMethodB(self) end
-		__Abstract__() function objMethodA(self) end
-		__Final__() function objMethodB(self) end
-		function objMethodC(self) end
-	end)
+        function __new(self, tbl)
+            if type(tbl) == "table" and tbl.name then
+                return tbl, true
+            end
+        end
 
-	class "A" (function(_ENV)
-		function objMethodB(self) end
-		function objMethodC(self) end
-	end)
+        function __ctor(self, name)
+            if name then self.name = name end
+            created[self.name] = self
+        end
 
-	class "C" { IA, A }
+        function __exist(_, name)
+            return created[name]
+        end
+    end)
 
-	Assert.Nil(C.ifMethodB)
-	Assert.Nil(C.objMethodB)
+    ann = { name = "Ann" }
+    obj = User(ann)
+    Assert.Equal(ann, obj)
+    Assert.Equal(obj, User("Ann"))
+    Assert.Equal("anonymous", User().name)
+end
 
-	local obj = C()
-	Assert.Equal(IA.objMethodB, obj.objMethodB)
-	Assert.Equal(A.objMethodC, obj.objMethodC)
+__Test__() function order()
+    interface "IName" (function(self)
+        __Abstract__() function SetName(self) end
+
+        __Abstract__() function GetName(self) end
+
+        -- initializer
+        function __init(self) Assert.Step("IName Init") end
+
+        -- destructor
+        function __dtor(self) Assert.Step("IName Dispose") end
+    end)
+
+    interface "IAge" (function(self)
+        __Abstract__() function SetAge(self) end
+
+        __Abstract__() function GetAge(self) end
+
+        -- initializer
+        function __init(self) Assert.Step("IAge Init") end
+
+        -- destructor
+        function __dtor(self) Assert.Step("IAge Dispose") end
+    end)
+
+    class "Person" (function(_ENV)
+        extend "IName" "IAge"
+
+        function __ctor(self) Assert.Step("Person Init") end
+
+        function __dtor(self) Assert.Step("Person Dispose") end
+    end)
+
+    class "Student" (function(_ENV)
+        inherit "Person"
+
+        function __ctor(self) super(self) Assert.Step("Student Init") end
+
+        function __dtor(self) Assert.Step("Student Dispose") end
+    end)
+
+    o = Student()
+    o:Dispose()
+
+    Assert.Same(Assert.GetSteps(), {
+        "Person Init", "Student Init", "IName Init", "IAge Init",
+        "IAge Dispose", "IName Dispose", "Student Dispose", "Person Dispose"
+    })
+end
+
+__Test__() function metamethod()
+    class "Range" (function(_ENV)
+        function __new(_, min, max)
+            return { min = min, max = max }, true
+        end
+
+        function __tostring(self)
+            return "[" .. self.min .. "," .. self.max .. "]"
+        end
+
+        function __add(self, tar)
+            return Range(math.min(self.min, tar.min), math.max(self.max, tar.max))
+        end
+    end)
+
+    Assert.Equal(tostring(Range(1, 3) + Range(2, 4)), "[1,4]")
+end
+
+__Test__() function method()
+    interface "IA" (function(_ENV)
+        __Static__() function ifMethodB(self) end
+        __Abstract__() function objMethodA(self) end
+        __Final__() function objMethodB(self) end
+        function objMethodC(self) end
+    end)
+
+    class "A" (function(_ENV)
+        function objMethodB(self) end
+        function objMethodC(self) end
+    end)
+
+    class "C" { IA, A }
+
+    Assert.Nil(C.ifMethodB)
+    Assert.Nil(C.objMethodB)
+
+    local obj = C()
+    Assert.Equal(IA.objMethodB, obj.objMethodB)
+    Assert.Equal(A.objMethodC, obj.objMethodC)
+end
+
+__Test__() function multiver()
+    class "MA" (function(_ENV)
+        function Test(self)
+            Assert.Step("Old")
+        end
+    end)
+
+    local obj       = MA()
+
+    class "MA" (function(_ENV)
+        function Test(self)
+            Assert.Step("New")
+        end
+    end)
+
+    obj:Test()
+
+    Assert.Same({ "Old" }, Assert.GetSteps())
+end
+
+__Test__() function appendmethod()
+    class "AP" {}
+
+    local obj       = AP()
+
+    function AP:Test()
+        Assert.Step("Test AP")
+    end
+
+    obj:Test()
+
+    Assert.Same({ "Test AP" }, Assert.GetSteps())
+end
+
+__Test__() function inheritance()
+    class "HA" (function(_ENV)
+        __Final__() function __ctor(self, ...)
+            Assert.Step("MA Ctor")
+            Class.GetNormalMetaMethod(Class.GetObjectClass(self), "__ctor")(self, ...)
+        end
+
+        __Final__() function Test(self, ...)
+            Assert.Step("MA Test")
+            Class.GetNormalMethod(Class.GetObjectClass(self), "Test")(self, ...)
+        end
+    end)
+
+    class "HB" (function(_ENV)
+        inherit "HA"
+
+        function __ctor(self, ...)
+            Assert.Step("MB Ctor")
+        end
+
+        function Test(self, ...)
+            Assert.Step("MB Test")
+        end
+    end)
+
+    class "HC" { HB }
+
+    local obj       = HC()
+
+    Assert.Equal(obj.Test, HA.Test)
+
+    obj:Test()
+
+    Assert.Same(Assert.GetSteps(), {
+        "MA Ctor", "MB Ctor", "MA Test", "MB Test"
+    })
+end
+
+__Test__() function event()
+    class "EA" (function(_ENV)
+        __EventChangeHandler__(function(delegate, owner, name)
+            Assert.Step("Change " .. name)
+        end)
+        event "OnTest"
+
+        __Static__() event "OnObjectCreated"
+
+        function Fire(self, ...) OnTest(self, ...) end
+
+        function __ctor(self)
+            OnObjectCreated(self)
+        end
+    end)
+
+    EA.OnObjectCreated = function(obj) Assert.Step("Created") end
+
+    local obj       = EA()
+
+    Assert.Same(Assert.GetSteps(), { "Created" })
+    Assert.ResetSteps()
+
+    function obj:OnTest() Assert.Step("Final") end
+
+    obj.OnTest      = obj.OnTest + function(self) Assert.Step("Stack") end
+
+    obj:Fire()
+    Assert.Same(Assert.GetSteps(), { "Change OnTest", "Change OnTest", "Stack", "Final" })
+    Assert.ResetSteps()
+
+    local blockhandler = function(self) Assert.Step("Block") return true end
+
+    obj.OnTest      = obj.OnTest + blockhandler
+
+    obj:Fire()
+    Assert.Same(Assert.GetSteps(), { "Change OnTest", "Stack", "Block" })
+    Assert.ResetSteps()
+
+    obj.OnTest:SetInitFunction(function(self) Assert.Step("Init") return true end)
+
+    obj:Fire()
+    Assert.Same(Assert.GetSteps(), { "Change OnTest", "Init" })
+    Assert.ResetSteps()
+
+    obj.OnTest:SetInitFunction(nil)
+    obj.OnTest      = obj.OnTest - blockhandler
+
+    obj:Fire()
+    Assert.Same(Assert.GetSteps(), { "Change OnTest", "Change OnTest", "Stack", "Final" })
 end
