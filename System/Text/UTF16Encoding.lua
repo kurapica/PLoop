8ec7f4dcb4732192158fe4efbd80ba77e9e0fcd2
kurapica <kurapica125@outlook.com>
2020-08-14 10:00:32 +0800

    Revert "Revert "Simple the encode&decode with Toolset bitwise api""
    This reverts commit 9d7fd406b935c0f8a65b086e8ca6923d3c261b06.

--- System/Text/UTF16Encoding.lua
+++ System/Text/UTF16Encoding.lua
@@ -28,6 +28,10 @@ PLoop(function(_ENV)
         loadsnippet             = Toolset.loadsnippet,
         error                   = error,
 
+        band                    = Toolset.band,
+        lshift                  = Toolset.lshift,
+        rshift                  = Toolset.rshift,
+
         -- Declare here
         decodeLE                = false,
         encodeLE                = false,
@@ -35,7 +39,6 @@ PLoop(function(_ENV)
         encodeBE                = false,
     }
 
-    -- Default
     function decodeLE(str, startp)
         startp                  = startp or 1
 
@@ -44,14 +47,14 @@ PLoop(function(_ENV)
 
         if obyte <= 0xD7 or obyte >= 0xE0 then
             -- two bytes
-            return obyte * 0x100 + sbyte, 2
+            return lshift(obyte, 8) + sbyte, 2
         elseif obyte >= 0xD8 and obyte <= 0xDB then
             -- four byte
             local fbyte, tbyte  = strbyte(str, startp + 2, startp + 3)
             if not tbyte or not fbyte then return nil end
 
             if tbyte >= 0xDC and tbyte <= 0xDF then
-                return ((obyte - 0xD8) * 0x100 + sbyte) * 0x400 + ((tbyte - 0xDC) * 0x100 + fbyte) + 0x10000, 4
+                return lshift((lshift((obyte - 0xD8), 8) + sbyte), 10) + (lshift((tbyte - 0xDC), 8) + fbyte) + 0x10000, 4
             else
                 return nil
             end
@@ -65,21 +68,21 @@ PLoop(function(_ENV)
             -- 2
             if code <= 0xD7FF or (code >= 0xE000 and code <= 0xFFFF) then
                 return strchar(
-                    code % 0x100,
-                    floor(code / 0x100)
+                    band(code, 0xff),
+                    rshift(code, 8)
                 )
             end
 
             -- 4 surrogate pairs
             if code >= 0x10000 and code <= 0x10FFFF then
                 code            = code - 0x10000
-                local high      = floor(code / 0x400)
-                local low       = code % 0x400
+                local high      = rshift(code, 10)
+                local low       = band(code, 0x3ff)
                 return strchar(
-                    high % 0x100,
-                    0xD8 + floor(high / 0x100),
-                    low % 0x100,
-                    0xDC + floor(low / 0x100)
+                    band(high, 0xff),
+                    0xD8 + rshift(high, 8),
+                    band(low, 0xff),
+                    0xDC + rshift(low, 8)
                 )
             end
         end
@@ -95,14 +98,14 @@ PLoop(function(_ENV)
 
         if obyte <= 0xD7 or obyte >= 0xE0 then
             -- two bytes
-            return obyte * 0x100 + sbyte, 2
+            return lshift(obyte, 8) + sbyte, 2
         elseif obyte >= 0xD8 and obyte <= 0xDB then
             -- four byte
             local tbyte, fbyte  = strbyte(str, startp + 2, startp + 3)
             if not tbyte or not fbyte then return nil end
 
             if tbyte >= 0xDC and tbyte <= 0xDF then
-                return ((obyte - 0xD8) * 0x100 + sbyte) * 0x400 + ((tbyte - 0xDC) * 0x100 + fbyte) + 0x10000, 4
+                return lshift((lshift((obyte - 0xD8), 8) + sbyte), 10) + (lshift((tbyte - 0xDC), 8) + fbyte) + 0x10000, 4
             else
                 return nil
             end
@@ -116,245 +119,26 @@ PLoop(function(_ENV)
             -- 2
             if code <= 0xD7FF or (code >= 0xE000 and code <= 0xFFFF) then
                 return strchar(
-                    floor(code / 0x100),
-                    code % 0x100
+                    rshift(code, 8),
+                    band(code, 0xff)
                 )
             end
 
             -- 4 surrogate pairs
             if code >= 0x10000 and code <= 0x10FFFF then
                 code            = code - 0x10000
-                local high      = floor(code / 0x400)
-                local low       = code % 0x400
+                local high      = rshift(code, 10)
+                local low       = band(code, 0x3ff)
                 return strchar(
-                    0xD8 + floor(high / 0x100),
-                    high % 0x100,
-                    0xDC + floor(low / 0x100),
-                    low % 0x100
+                    0xD8 + rshift(high, 8),
+                    band(high, 0xff),
+                    0xDC + rshift(low, 8),
+                    band(low, 0xff)
                 )
             end
         end
 
         error(("%s is not a valid unicode."):format(code), 2)
-    end
-
-    -- Lua 5.3 - bitwise oper
-    if LUA_VERSION >= 5.3 then
-        -- Use load since 5.1 & 5.2 can't read the bitwise oper
-        decodeLE, encodeLE, decodeBE, encodeBE = loadsnippet([[
-            return function (str, startp)
-                startp              = startp or 1
-
-                local sbyte, obyte  = strbyte(str, startp, startp + 1)
-                if not obyte or not sbyte then return nil end
-
-                if obyte <= 0xD7 or obyte >= 0xE0 then
-                    -- two bytes
-                    return (obyte << 8) + sbyte, 2
-                elseif obyte >= 0xD8 and obyte <= 0xDB then
-                    -- four byte
-                    local fbyte, tbyte = strbyte(str, startp + 2, startp + 3)
-                    if not tbyte or not fbyte then return nil end
-
-                    if tbyte >= 0xDC and tbyte <= 0xDF then
-                        return ((((obyte - 0xD8) << 8) + sbyte) << 10) + (((tbyte - 0xDC) << 8) + fbyte) + 0x10000, 4
-                    else
-                        return nil
-                    end
-                else
-                    return nil
-                end
-            end,
-
-            function (code)
-                if code >= 0 then
-                    -- 2
-                    if code <= 0xD7FF or (code >= 0xE000 and code <= 0xFFFF) then
-                        return strchar(
-                            code & 0xff,
-                            code >> 8
-                        )
-                    end
-
-                    -- 4 surrogate pairs
-                    if code >= 0x10000 and code <= 0x10FFFF then
-                        code        = code - 0x10000
-                        local high  = code >> 10
-                        local low   = code & 0x3ff
-                        return strchar(
-                            high & 0xff,
-                            0xD8 + high >> 8,
-                            low & 0xff,
-                            0xDC + low >> 8
-                        )
-                    end
-                end
-
-                error(("%s is not a valid unicode."):format(code), 2)
-            end,
-
-            function (str, startp)
-                startp              = startp or 1
-
-                local obyte, sbyte  = strbyte(str, startp, startp + 1)
-                if not obyte or not sbyte then return nil end
-
-                if obyte <= 0xD7 or obyte >= 0xE0 then
-                    -- two bytes
-                    return (obyte << 8) + sbyte, 2
-                elseif obyte >= 0xD8 and obyte <= 0xDB then
-                    -- four byte
-                    local tbyte, fbyte = strbyte(str, startp + 2, startp + 3)
-                    if not tbyte or not fbyte then return nil end
-
-                    if tbyte >= 0xDC and tbyte <= 0xDF then
-                        return ((((obyte - 0xD8) << 8) + sbyte) << 10) + (((tbyte - 0xDC) << 8) + fbyte) + 0x10000, 4
-                    else
-                        return nil
-                    end
-                else
-                    return nil
-                end
-            end,
-
-            function (code)
-                if code >= 0 then
-                    -- 2
-                    if code <= 0xD7FF or (code >= 0xE000 and code <= 0xFFFF) then
-                        return strchar(
-                            code >> 8,
-                            code & 0xff
-                        )
-                    end
-
-                    -- 4 surrogate pairs
-                    if code >= 0x10000 and code <= 0x10FFFF then
-                        code        = code - 0x10000
-                        local high  = code >> 10
-                        local low   = code & 0x3ff
-                        return strchar(
-                            0xD8 + high >> 8,
-                            high & 0xff,
-                            0xDC + low >> 8,
-                            low & 0xff
-                        )
-                    end
-                end
-
-                error(("%s is not a valid unicode."):format(code), 2)
-            end
-        ]], "UTF16_ENCODE_DECODE", _ENV)()
-    end
-
-    -- Lua 5.2 - bit32 lib or luajit bit lib
-    if (LUA_VERSION == 5.2 and type(_G.bit32) == "table") or (LUA_VERSION == 5.1 and type(_G.bit) == "table") then
-        export {
-            band                = _G.bit32 and bit32.band or bit.band,
-            lshift              = _G.bit32 and bit32.lshift or bit.lshift,
-            rshift              = _G.bit32 and bit32.rshift or bit.rshift,
-        }
-
-        function decodeLE(str, startp)
-            startp              = startp or 1
-
-            local sbyte, obyte  = strbyte(str, startp, startp + 1)
-            if not obyte or not sbyte then return nil end
-
-            if obyte <= 0xD7 or obyte >= 0xE0 then
-                -- two bytes
-                return lshift(obyte, 8) + sbyte, 2
-            elseif obyte >= 0xD8 and obyte <= 0xDB then
-                -- four byte
-                local fbyte, tbyte = strbyte(str, startp + 2, startp + 3)
-                if not tbyte or not fbyte then return nil end
-
-                if tbyte >= 0xDC and tbyte <= 0xDF then
-                    return lshift((lshift((obyte - 0xD8), 8) + sbyte), 10) + (lshift((tbyte - 0xDC), 8) + fbyte) + 0x10000, 4
-                else
-                    return nil
-                end
-            else
-                return nil
-            end
-        end
-
-        function encodeLE(code)
-            if code >= 0 then
-                -- 2
-                if code <= 0xD7FF or (code >= 0xE000 and code <= 0xFFFF) then
-                    return strchar(
-                        band(code, 0xff),
-                        rshift(code, 8)
-                    )
-                end
-
-                -- 4 surrogate pairs
-                if code >= 0x10000 and code <= 0x10FFFF then
-                    code        = code - 0x10000
-                    local high  = rshift(code, 10)
-                    local low   = band(code, 0x3ff)
-                    return strchar(
-                        band(high, 0xff),
-                        0xD8 + rshift(high, 8),
-                        band(low, 0xff),
-                        0xDC + rshift(low, 8)
-                    )
-                end
-            end
-
-            error(("%s is not a valid unicode."):format(code), 2)
-        end
-
-        function decodeBE(str, startp)
-            startp              = startp or 1
-
-            local obyte, sbyte  = strbyte(str, startp, startp + 1)
-            if not obyte or not sbyte then return nil end
-
-            if obyte <= 0xD7 or obyte >= 0xE0 then
-                -- two bytes
-                return lshift(obyte, 8) + sbyte, 2
-            elseif obyte >= 0xD8 and obyte <= 0xDB then
-                -- four byte
-                local tbyte, fbyte = strbyte(str, startp + 2, startp + 3)
-                if not tbyte or not fbyte then return nil end
-
-                if tbyte >= 0xDC and tbyte <= 0xDF then
-                    return lshift((lshift((obyte - 0xD8), 8) + sbyte), 10) + (lshift((tbyte - 0xDC), 8) + fbyte) + 0x10000, 4
-                else
-                    return nil
-                end
-            else
-                return nil
-            end
-        end
-
-        function encodeBE(code)
-            if code >= 0 then
-                -- 2
-                if code <= 0xD7FF or (code >= 0xE000 and code <= 0xFFFF) then
-                    return strchar(
-                        rshift(code, 8),
-                        band(code, 0xff)
-                    )
-                end
-
-                -- 4 surrogate pairs
-                if code >= 0x10000 and code <= 0x10FFFF then
-                    code        = code - 0x10000
-                    local high  = rshift(code, 10)
-                    local low   = band(code, 0x3ff)
-                    return strchar(
-                        0xD8 + rshift(high, 8),
-                        band(high, 0xff),
-                        0xDC + rshift(low, 8),
-                        band(low, 0xff)
-                    )
-                end
-            end
-
-            error(("%s is not a valid unicode."):format(code), 2)
-        end
     end
 
    --- Represents the utf-16 encoding with little-endian.
--- System/Text/UTF8Encoding.lua
+++ System/Text/UTF8Encoding.lua
@@ -30,12 +30,11 @@ PLoop(function(_ENV)
         loadsnippet             = Toolset.loadsnippet,
         error                   = error,
 
-        -- declared here so they are global variables
-        decode                  = false,
-        encode                  = false,
+        band                    = Toolset.band,
+        lshift                  = Toolset.lshift,
+        rshift                  = Toolset.rshift,
     }
 
-    -- Default
     function decode(str, startp)
         startp                  = startp or 1
 
@@ -51,30 +50,28 @@ PLoop(function(_ENV)
         elseif byte < 0xE0 then
             -- 2-byte
             local sbyte         = strbyte(str, startp + 1)
-            if not sbyte or floor(sbyte / 0x40) ~= 2 then
+            if not sbyte or band(sbyte, 0xC0) ~= 0x80 then
                 -- Error
                 return byte + 0xDC00, 1
             end
-            return (byte * 0x40) + sbyte - 0x3080, 2
+            return lshift(byte, 6) + sbyte - 0x3080, 2
         elseif byte < 0xF0 then
             -- 3-byte
             local sbyte, tbyte  = strbyte(str, startp + 1, startp + 2)
-            if not (sbyte and tbyte) or floor(sbyte / 0x40) ~= 2 or (byte == 0xE0 and sbyte < 0xA0) or floor(tbyte / 0x40) ~= 2 then
+            if not (sbyte and tbyte) or band(sbyte, 0xC0) ~= 0x80 or (byte == 0xE0 and sbyte < 0xA0) or band(tbyte, 0xC0) ~= 0x80 then
                 -- Error
                 return byte + 0xDC00, 1
             end
-            return (byte * 0x1000) + (sbyte * 0x40) + tbyte - 0xE2080, 3
+            return lshift(byte, 12) + lshift(sbyte, 6) + tbyte - 0xE2080, 3
         elseif byte < 0xF5 then
             -- 4-byte
             local sbyte, tbyte, fbyte = strbyte(str, startp + 1, startp + 3)
-            if not (sbyte and tbyte and fbyte) or floor(sbyte / 0x40) ~= 2 or (byte == 0xF0 and sbyte < 0x90) or (byte == 0xF4 and sbyte >= 0x90) or floor(tbyte / 0x40) ~= 2 or floor(fbyte / 0x40) ~= 2 then
+            if not (sbyte and tbyte and fbyte) or band(sbyte, 0xC0) ~= 0x80 or (byte == 0xF0 and sbyte < 0x90) or (byte == 0xF4 and sbyte >= 0x90) or band(tbyte, 0xC0) ~= 0x80 or band(fbyte, 0xC0) ~= 0x80 then
                 -- Error
                 return byte + 0xDC00, 1
             end
-
-            return (byte * 0x40000) + (sbyte * 0x1000) + (tbyte * 0x40) + fbyte - 0x3C82080, 4
+            return lshift(byte, 18) + lshift(sbyte, 12) + lshift(tbyte, 6) + fbyte - 0x3C82080, 4
         else
-            -- Error
             return byte + 0xDC00, 1
         end
     end
@@ -87,203 +84,32 @@ PLoop(function(_ENV)
             -- 2
             if code <= 0x7FF then
                 return strchar(
-                    floor(code / 0x40) + 0xC0,
-                    code % 0x40 + 0x80
+                    rshift(code, 6) + 0xC0,
+                    band(code, 0x3F) + 0x80
                 )
             end
 
             -- 3
             if code <= 0xFFFF then
                 return strchar(
-                    floor(code / 0x1000) + 0xE0,
-                    floor(code / 0x40) % 0x40 + 0x80,
-                    code % 0x40 + 0x80
+                    rshift(code, 12) + 0xE0,
+                    band(rshift(code, 6), 0x3F) + 0x80,
+                    band(code, 0x3F) + 0x80
                 )
             end
 
             -- 4
             if code <= 0x1FFFFF then
                 return strchar(
-                    floor(code / 0x40000) + 0xF0,
-                    floor(code / 0x1000) % 0x40 + 0x80,
-                    floor(code / 0x40) % 0x40 + 0x80,
-                    code % 0x40 + 0x80
+                    rshift(code, 18) + 0xF0,
+                    band(rshift(code, 12), 0x3F) + 0x80,
+                    band(rshift(code, 6), 0x3F) + 0x80,
+                    band(code, 0x3F) + 0x80
                 )
             end
         end
 
-        error(("%s is not a valid unicode."):format(code), 2)
-    end
-
-    -- Lua 5.3 - bitwise oper
-    if LUA_VERSION >= 5.3 then
-        -- Use load since 5.1 & 5.2 can't read the bitwise oper
-        decode = loadsnippet([[
-            return function (str, startp)
-                startp          = startp or 1
-
-                local byte      = strbyte(str, startp)
-                if not byte then return nil end
-
-                if byte < 0x80 then
-                    -- 1-byte
-                    return byte, 1
-                elseif byte < 0xC2 then
-                    -- Error
-                    return byte + 0xDC00, 1
-                elseif byte < 0xE0 then
-                    -- 2-byte
-                    local sbyte = strbyte(str, startp + 1)
-                    if not sbyte or (sbyte & 0xC0) ~= 0x80 then
-                        -- Error
-                        return byte + 0xDC00, 1
-                    end
-                    return (byte << 6) + sbyte - 0x3080, 2
-                elseif byte < 0xF0 then
-                    -- 3-byte
-                    local sbyte, tbyte = strbyte(str, startp + 1, startp + 2)
-                    if not (sbyte and tbyte) or (sbyte & 0xC0) ~= 0x80 or (byte == 0xE0 and sbyte < 0xA0) or (tbyte & 0xC0) ~= 0x80 then
-                        -- Error
-                        return byte + 0xDC00, 1
-                    end
-                    return (byte << 12) + (sbyte << 6) + tbyte - 0xE2080, 3
-                elseif byte < 0xF5 then
-                    -- 4-byte
-                    local sbyte, tbyte, fbyte = strbyte(str, startp + 1, startp + 3)
-                    if not (sbyte and tbyte and fbyte) or (sbyte & 0xC0) ~= 0x80 or (byte == 0xF0 and sbyte < 0x90) or (byte == 0xF4 and sbyte >= 0x90) or (tbyte & 0xC0) ~= 0x80 or (fbyte & 0xC0) ~= 0x80 then
-                        -- Error
-                        return byte + 0xDC00, 1
-                    end
-                    return (byte << 18) + (sbyte << 12) + (tbyte << 6) + fbyte - 0x3C82080, 4
-                else
-                    return byte + 0xDC00, 1
-                end
-            end
-        ]], "UTF8_DECODE", _ENV)()
-
-        encode = loadsnippet([[
-            return function (code)
-                if code >= 0 then
-                    -- 1
-                    if code <= 0x7F then return strchar( code ) end
-
-                    -- 2
-                    if code <= 0x7FF then
-                        return strchar(
-                            (code >> 6) + 0xC0,
-                            code & 0x3F + 0x80
-                        )
-                    end
-
-                    -- 3
-                    if code <= 0xFFFF then
-                        return strchar(
-                            (code >> 12) + 0xE0,
-                            (code >> 6) & 0x3F + 0x80,
-                            code & 0x3F + 0x80
-                        )
-                    end
-
-                    -- 4
-                    if code <= 0x1FFFFF then
-                        return strchar(
-                            (code >> 18) + 0xF0,
-                            (code >> 12) & 0x3F + 0x80,
-                            (code >> 6) & 0x3F + 0x80,
-                            code & 0x3F + 0x80
-                        )
-                    end
-                end
-
-                error(("%s is not a valid code_point."):format(code), 2)
-            end
-        ]], "UTF8_ENCODE", _ENV)()
-    end
-
-    -- Lua 5.2 - bit32 lib or luajit bit lib
-    if (LUA_VERSION == 5.2 and type(_G.bit32) == "table") or (LUA_VERSION == 5.1 and type(_G.bit) == "table") then
-        export {
-            band                = _G.bit32 and bit32.band   or bit.band,
-            lshift              = _G.bit32 and bit32.lshift or bit.lshift,
-            rshift              = _G.bit32 and bit32.rshift or bit.rshift,
-        }
-
-        function decode(str, startp)
-            startp              = startp or 1
-
-            local byte          = strbyte(str, startp)
-            if not byte then return nil end
-
-            if byte < 0x80 then
-                -- 1-byte
-                return byte, 1
-            elseif byte < 0xC2 then
-                -- Error
-                return byte + 0xDC00, 1
-            elseif byte < 0xE0 then
-                -- 2-byte
-                local sbyte     = strbyte(str, startp + 1)
-                if not sbyte or band(sbyte, 0xC0) ~= 0x80 then
-                    -- Error
-                    return byte + 0xDC00, 1
-                end
-                return lshift(byte, 6) + sbyte - 0x3080, 2
-            elseif byte < 0xF0 then
-                -- 3-byte
-                local sbyte, tbyte = strbyte(str, startp + 1, startp + 2)
-                if not (sbyte and tbyte) or band(sbyte, 0xC0) ~= 0x80 or (byte == 0xE0 and sbyte < 0xA0) or band(tbyte, 0xC0) ~= 0x80 then
-                    -- Error
-                    return byte + 0xDC00, 1
-                end
-                return lshift(byte, 12) + lshift(sbyte, 6) + tbyte - 0xE2080, 3
-            elseif byte < 0xF5 then
-                -- 4-byte
-                local sbyte, tbyte, fbyte = strbyte(str, startp + 1, startp + 3)
-                if not (sbyte and tbyte and fbyte) or band(sbyte, 0xC0) ~= 0x80 or (byte == 0xF0 and sbyte < 0x90) or (byte == 0xF4 and sbyte >= 0x90) or band(tbyte, 0xC0) ~= 0x80 or band(fbyte, 0xC0) ~= 0x80 then
-                    -- Error
-                    return byte + 0xDC00, 1
-                end
-                return lshift(byte, 18) + lshift(sbyte, 12) + lshift(tbyte, 6) + fbyte - 0x3C82080, 4
-            else
-                return byte + 0xDC00, 1
-            end
-        end
-
-        function encode(code)
-            if code >= 0 then
-                -- 1
-                if code <= 0x7F then return strchar( code ) end
-
-                -- 2
-                if code <= 0x7FF then
-                    return strchar(
-                        rshift(code, 6) + 0xC0,
-                        band(code, 0x3F) + 0x80
-                    )
-                end
-
-                -- 3
-                if code <= 0xFFFF then
-                    return strchar(
-                        rshift(code, 12) + 0xE0,
-                        band(rshift(code, 6), 0x3F) + 0x80,
-                        band(code, 0x3F) + 0x80
-                    )
-                end
-
-                -- 4
-                if code <= 0x1FFFFF then
-                    return strchar(
-                        rshift(code, 18) + 0xF0,
-                        band(rshift(code, 12), 0x3F) + 0x80,
-                        band(rshift(code, 6), 0x3F) + 0x80,
-                        band(code, 0x3F) + 0x80
-                    )
-                end
-            end
-
-            error(("%s is not a valid code_point."):format(code), 2)
-        end
+        error(("%s is not a valid code_point."):format(code), 2)
     end
 
     --[[
